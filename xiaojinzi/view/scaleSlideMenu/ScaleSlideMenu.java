0e2e3fdbeacc30f68a6e3816b07ca8905b587073
xiaojinzi <347837667@qq.com>
2016-05-17 14:50:05 +0800

    fix some bug

--- xiaojinzi/view/scaleSlideMenu/ScaleSlideMenu.java
+++ xiaojinzi/view/scaleSlideMenu/ScaleSlideMenu.java
@@ -2,6 +2,7 @@ package xiaojinzi.view.scaleSlideMenu;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
@@ -25,11 +26,10 @@ import xiaojinzi.view.common.RectEntity;
  *         1.如果是两个孩子的情况
  *         第一个孩子是菜单,默认是左边
  *         第二个是主界面
- *         2.如果是三个孩子的
- *         第一个孩子是左边的菜单
- *         第二个孩子是右边的菜单
- *         第三个孩子是主界面
- *         3.超过三个孩子或者小于两个孩子都会报错
+ *         3.不是两个孩子都会报错
+ *
+ *         1.用到的类:RectEntity
+ *                    保存位置信息
  */
 public class ScaleSlideMenu extends ViewGroup {
 
@@ -43,7 +43,18 @@ public class ScaleSlideMenu extends ViewGroup {
     /**
      * 最小的距离生效的百分比
      */
-    public static final float MIN_SLIDEPERCENT = 0.2f;
+    public static final float MIN_SLIDEPERCENT = 0.1f;
+
+    /**
+     * 最小的菜单宽度占用百分比
+     */
+    public static final float MIN_MENUPERCENT = 0.5f;
+
+    /**
+     * 最大的菜单宽度的占用百分比
+     */
+    public static final float MAX_MENUPERCENT = 0.7f;
+
 
     /**
      * 侧滑的时候,当触摸的x距离屏幕小于屏幕宽度的10%的时候会生效
@@ -51,9 +62,26 @@ public class ScaleSlideMenu extends ViewGroup {
     public static final float DEFALUT_SLIDEPERCENT = MIN_SLIDEPERCENT;
 
     /**
-     * 普通的模式
+     * 滑动的时候是一个普通的模式
      */
     public static final int NORMAL_SLIDE_MODE = 0;
+
+    /**
+     * 滑动的时候是一个缩放的模式
+     */
+    public static final int SCALE_SLIDE_MODE = 1;
+
+
+    /**
+     * 菜单在左边
+     */
+    public static final int MENU_GRAVITY_LEFT = 0;
+
+
+    /**
+     * 菜单在右边
+     */
+    public static final int MENU_GRAVITY_RIGHT = 1;
 
     //=================================常量 end =====================================
 
@@ -79,15 +107,9 @@ public class ScaleSlideMenu extends ViewGroup {
         this.context = context;
         scroller = new Scroller(context);
         vt = VelocityTracker.obtain();
-        screenWidth = ScreenUtils.getScreenWidth(context);
     }
 
     //======================================成员变量 start===========================================
-
-    /**
-     * 屏幕的宽度
-     */
-    private int screenWidth;
 
     /**
      * 上下文对象
@@ -120,6 +142,26 @@ public class ScaleSlideMenu extends ViewGroup {
     private boolean preIsMenuOpen;
 
     /**
+     * 侧滑的模式,默认是缩放的模式
+     */
+    private int slideMode = SCALE_SLIDE_MODE;
+
+    /**
+     * 菜单的位置在左边还是在右边
+     */
+    private int menuGravity = MENU_GRAVITY_RIGHT;
+
+    /**
+     * 侧滑菜单生效的距离百分比
+     */
+    private float slidePercent = DEFALUT_SLIDEPERCENT;
+
+    /**
+     * 菜单宽度占屏幕的比例,默认是最小的
+     */
+    private float menuPercent = MAX_MENUPERCENT;
+
+    /**
      * 自身的宽
      */
     private int mWidth;
@@ -135,14 +177,52 @@ public class ScaleSlideMenu extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec));
+
+        //检查孩子的个数
+        checkChildCount();
+
+        //宽度的计算模式
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        //推荐的宽度的值
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        //高度的计算模式
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        //推荐的高度的值
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        //菜单
+        View menuView = getChildAt(0);
+        //生成菜单的宽度的计算模式和大小,菜单的宽度大小受一个百分比限制
+        int menuWidthSpec = MeasureSpec.makeMeasureSpec((int) (widthSize * menuPercent), MeasureSpec.AT_MOST);
+        //生成菜单的高度的计算模式和大小
+        int menuHeightSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.AT_MOST);
+        //让菜单的试图部分去测量自己
+        menuView.measure(menuWidthSpec, menuHeightSpec);
+
+
+        //主界面
+        View mainView = getChildAt(1);
+        //生成主界面的宽度的计算模式和大小
+        int mainWidthSpec = MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.AT_MOST);
+        //生成主界面的高度的计算模式和大小
+        int mainHeightSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.AT_MOST);
+        //让主界面的试图部分去测量自己
+        mainView.measure(mainWidthSpec, mainHeightSpec);
+
+        //侧滑控件的大小呢就直接采用父容器推荐的，所以
+        //ViewGroup.LayoutParams.WRAP_CONTENT 和 ViewGroup.LayoutParams.MATCH_PARENT 的情况下大小是一致的,这里做一个说明
+        //作用是保存自身的大小,没有这句话控件不显示,也就是看不到啦
+        setMeasuredDimension(widthSize, heightSize);
+
+        //保存自身的宽度和高度,其他地方有用
         mWidth = getWidth();
         mHeight = getHeight();
     }
 
     @Override
     public void computeScroll() { //当View完成滚动的时候调用
+        //如果轨迹中还有没有滚完的点
         if (scroller.computeScrollOffset()) {
             removeRepeatData();//消除重复数据
             //如果完成了滑动
@@ -170,8 +250,19 @@ public class ScaleSlideMenu extends ViewGroup {
                 int x = (int) ev.getX();
 
                 //拦截菜单打开的情况下主界面的事件
-                if (isMenuOpen && x > Math.abs(rectEntities.get(0).leftX)) {
-                    return true;
+                if (isMenuOpen) {
+                    //菜单的位置参数对象
+                    RectEntity menuRect = rectEntities.get(0);
+
+                    if (menuGravity == MENU_GRAVITY_LEFT) { //如果菜单在左边
+                        if (x > Math.abs(menuRect.leftX)) { //满足说明了点击的是主界面的区域
+                            return true;
+                        }
+                    } else { //菜单在右边
+                        if (x < (mWidth - (menuRect.rightX - menuRect.leftX))) {
+                            return true;
+                        }
+                    }
                 }
         }
 
@@ -189,8 +280,6 @@ public class ScaleSlideMenu extends ViewGroup {
      */
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        //检查孩子个数
-        checkChildCount();
 
         //计算所有孩子的位置信息
         computePosition();
@@ -238,18 +327,26 @@ public class ScaleSlideMenu extends ViewGroup {
     @Override
     public boolean onTouchEvent(MotionEvent e) {
 
-        int action = e.getAction(); // screenWidth
+        //获取事件的类型(动作)
+        int action = e.getAction();
 
-        //如果按下的,进行一些判断
+        //如果按下的,进行有效点的判读,只有接近边缘小于某一个百分比,才可以滑出菜单
         if ((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
+            //拿到按下的时候的横左边
             float x = e.getX();
-            if (x / ((Number) screenWidth).floatValue() < slidePercent) { //如果距离边界的百分比小于设置的百分比
-                isMyEvent = true;
+            if (menuGravity == MENU_GRAVITY_LEFT) { //如果菜单在左边
+                if (x / ((Number) mWidth).floatValue() < slidePercent) { //如果距离边界的百分比小于设置的百分比
+                    isMyEvent = true;
+                }
+            } else {
+                if ((mWidth - x) / ((Number) mWidth).floatValue() < slidePercent) { //如果距离边界的百分比小于设置的百分比
+                    isMyEvent = true;
+                }
             }
         }
 
         //如果不需要侧滑出菜单,并且菜单是关闭状态
-        if (!isMyEvent && !isMenuOpen) {
+        if (!isMyEvent && !isMenuOpen && !isScrolling) {
             return false;
         }
 
@@ -260,6 +357,8 @@ public class ScaleSlideMenu extends ViewGroup {
             case MotionEvent.ACTION_DOWN: //按下
 
                 isMove = false;
+
+                //保存按下的时候的坐标
                 currentX = (int) e.getX();
                 currentY = (int) e.getY();
 
@@ -270,17 +369,23 @@ public class ScaleSlideMenu extends ViewGroup {
 
             case MotionEvent.ACTION_MOVE: //移动
 
+                //保存移动之后的新的坐标点
                 finalX = (int) e.getX();
                 finalY = (int) e.getY();
 
+                //如果新旧坐标不一致
                 if (finalX != currentX || finalY != currentY) {
+
+                    //计算水平距离和垂直距离
                     int dx = finalX - currentX;
                     int dy = finalY - currentY;
 
+                    //让整个试图跟着手指移动起来
                     scrollBy(-dx, 0);
 
                     scaleMainView();
 
+                    //移动之后旧的坐标进行更新
                     currentX = (int) e.getX();
                     currentY = (int) e.getY();
                     isMove = true;
@@ -295,10 +400,21 @@ public class ScaleSlideMenu extends ViewGroup {
                     //水平方向的速度
                     float xVelocity = vt.getXVelocity();
 
+                    //如果速度的绝对值大于200,我们就认为试图有一个抛出的感觉
+
+                    //如果速度达到了
                     if (xVelocity > 200) {
-                        openMenu();
+                        if (menuGravity == MENU_GRAVITY_LEFT) {
+                            openMenu();
+                        } else {
+                            closeMenu();
+                        }
                     } else if (xVelocity < -200) {
-                        closeMenu();
+                        if (menuGravity == MENU_GRAVITY_LEFT) {
+                            closeMenu();
+                        } else {
+                            openMenu();
+                        }
                     } else {
                         judgeShouldSmoothToLeftOrRight();
                     }
@@ -332,7 +448,6 @@ public class ScaleSlideMenu extends ViewGroup {
      */
     private void computePosition() {
         rectEntities.clear();
-
         //获取孩子的个数
         int childCount = getChildCount();
 
@@ -342,8 +457,14 @@ public class ScaleSlideMenu extends ViewGroup {
 
             //闯进第一个孩子的位置参数
             RectEntity menuRect = new RectEntity();
-            menuRect.rightX = 0;
-            menuRect.leftX = menuRect.rightX - menuView.getMeasuredWidth();
+            if (menuGravity == MENU_GRAVITY_LEFT) { //如果菜单是在左边的
+                menuRect.rightX = 0;
+                menuRect.leftX = menuRect.rightX - menuView.getMeasuredWidth();
+            } else {
+                menuRect.leftX = mWidth;
+                menuRect.rightX = menuRect.leftX + menuView.getMeasuredWidth();
+            }
+
             menuRect.leftY = 0;
             menuRect.rightY = menuRect.leftY + menuView.getMeasuredHeight();
 
@@ -373,8 +494,8 @@ public class ScaleSlideMenu extends ViewGroup {
      */
     private void checkChildCount() {
         int childCount = getChildCount();
-        if (childCount > 3 || childCount < 2) {
-            throw new RuntimeException("the childCount must be 2 or 3");
+        if (childCount != 2) {
+            throw new RuntimeException("the childCount must be 2");
         }
     }
 
@@ -399,7 +520,6 @@ public class ScaleSlideMenu extends ViewGroup {
      */
     private void smoothTo(int finalX) {
         isScrolling = true;
-//        scroller.abortAnimation();
         scroller.startScroll(getScrollX(), 0, finalX - getScrollX(), 0, defalutDuring);
         removeRepeatData();//消除重复数据
         scrollTo(scroller.getCurrX(), 0);
@@ -409,36 +529,47 @@ public class ScaleSlideMenu extends ViewGroup {
      * 缩放主界面
      */
     private void scaleMainView() {
-        float percent = ((Number) Math.abs(getScrollX())).floatValue() / ((Number) Math.abs(rectEntities.get(0).leftX)).floatValue();
-
-        getChildAt(1).setScaleX(1f - 0.4f * (percent));
-        getChildAt(1).setScaleY(1f - 0.4f * (percent));
+        if (slideMode == SCALE_SLIDE_MODE) { //如果模式是缩放的模式
+            float percent = ((Number) Math.abs(getScrollX())).floatValue() / ((Number) Math.abs(rectEntities.get(0).leftX)).floatValue();
+            getChildAt(1).setScaleX(1f - 0.4f * (percent));
+            getChildAt(1).setScaleY(1f - 0.4f * (percent));
+        }
     }
 
     /**
      * 判断当前的位置应该是滑出菜单还是关闭菜单
      */
     private void judgeShouldSmoothToLeftOrRight() {
-        //菜单的宽度
-        int menuLeftX = rectEntities.get(0).leftX;
-        //获取到当前的位置
-        int scrollX = getScrollX();
-        if (menuLeftX / 2 > scrollX) {
-            openMenu();
+        //菜单的位置参数对象
+        RectEntity menuRect = rectEntities.get(0);
+        if (menuGravity == MENU_GRAVITY_LEFT) { //如果菜单在左边
+            //菜单左边的横左边
+            int menuLeftX = menuRect.leftX;
+            //获取到当前的位置
+            int scrollX = getScrollX();
+            if (menuLeftX / 2 > scrollX) {
+                openMenu();
+            } else {
+                closeMenu();
+            }
         } else {
-            closeMenu();
+            //菜单的宽度
+            int menuWidth = menuRect.rightX - menuRect.leftX;
+            //获取到当前的位置
+            int scrollX = getScrollX();
+            if (menuWidth / 2 > scrollX) {
+                closeMenu();
+            } else {
+                openMenu();
+            }
         }
+
     }
 
     //======================================私有方法 end===========================================
 
 
     //============================================用户方法区域 start============================================
-
-    /**
-     * 侧滑菜单生效的距离百分比
-     */
-    private float slidePercent = DEFALUT_SLIDEPERCENT;
 
     /**
      * 设置侧滑的百分比,0-1
@@ -456,12 +587,48 @@ public class ScaleSlideMenu extends ViewGroup {
     }
 
     /**
+     * 获取侧滑的百分比
+     *
+     * @return
+     */
+    public float getSlidePercent() {
+        return slidePercent;
+    }
+
+    /**
+     * 设置侧滑的模式
+     *
+     * @param slideMode 侧滑的模式
+     *                  {@link ScaleSlideMenu#NORMAL_SLIDE_MODE}
+     *                  {@link ScaleSlideMenu#SCALE_SLIDE_MODE}
+     */
+    public void setSlideMode(int slideMode) {
+        this.slideMode = slideMode;
+    }
+
+    /**
+     * 获取侧滑的模式
+     *
+     * @return
+     */
+    public int getSlideMode() {
+        return slideMode;
+    }
+
+    /**
      * 打开菜单
      */
     public void openMenu() {
-        //拿到菜单的位置参数
-        RectEntity menuRect = rectEntities.get(0);
-        smoothTo(menuRect.leftX);
+        if (menuGravity == MENU_GRAVITY_LEFT) {
+            //拿到菜单的位置参数
+            RectEntity menuRect = rectEntities.get(0);
+            smoothTo(menuRect.leftX);
+        } else {
+            //拿到菜单的位置参数
+            RectEntity menuRect = rectEntities.get(0);
+            smoothTo(menuRect.rightX - menuRect.leftX);
+        }
+
         preIsMenuOpen = isMenuOpen;
         isMenuOpen = true;
     }
